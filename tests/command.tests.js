a3895849d7b5f15a7e3fd2ae3aae528f1f5449dc
Rhys Bartels-Waller <rhyslbw@gmail.com>
2016-01-31 17:12:35 +1100

    Add tests to check for default field assignment in Command

--- tests/command.tests.js
+++ tests/command.tests.js
@@ -1,11 +1,11 @@
-describe("Space.domain.Command", function() {
+describe('Space.domain.Command', function() {
 
-  beforeEach(function() {
-    this.params = { targetId: 'test' };
-    this.command = new Space.domain.Command(this.params);
-  });
+  describe('mixed in traits', function() {
 
-  describe("mixed in traits", function() {
+    beforeEach(function() {
+      this.params = { targetId: '123'};
+      this.command = new Space.domain.Command(this.params);
+    });
 
     it('is Ejsonable', function() {
       expect(this.command.hasMixin(Space.messaging.Ejsonable)).to.equal(true);
@@ -17,24 +17,38 @@ describe("Space.domain.Command", function() {
 
   });
 
-  it('defines its EJSON type correctly', function() {
-    expect(this.command.typeName()).to.equal('Space.domain.Command');
-  });
-
-  describe('default fields', function() {
-
-    it('can be created using string target id', function() {
+  describe('construction', function() {
+    
+    it('using string target id', function() {
       const createWithStringTargetId = function() {
         return new Space.domain.Command({targetId: '123'})
       };
       expect(createWithStringTargetId).not.to.throw
     });
 
-    it('can be created using Guid target id', function() {
+    it('using Guid target id', function() {
       const createWithGuidTargetId = function() {
         return new Space.domain.Command({targetId: new Guid()})
       };
       expect(createWithGuidTargetId).not.to.throw
+    });
+
+    it('defines its EJSON type correctly', function() {
+      expect(this.command.typeName()).to.equal('Space.domain.Command');
+    });
+    
+  });
+  
+  describe('default field assignment', function() {
+
+    beforeEach(function() {
+      this.params = { targetId: 'test' };
+      this.command = new Space.domain.Command(this.params);
+    });
+    
+    it('applies default fields', function() {
+      expect(this.command.targetId).to.equal(this.params.targetId);
+      expect(this.command.timestamp).to.be.a('date');
     });
 
   });
